5ce4c4a95d960009422d8d29d8725f2466847077
挺 江 <weskerjiang@hotmail.com>
2019-02-27 08:37:46 +0800

    新增主页

--- HSpider.Web/Startup.cs
+++ HSpider.Web/Startup.cs
@@ -15,6 +15,7 @@ namespace HSpider.Web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,9 +26,11 @@ namespace HSpider.Web
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            app.UseStaticFiles();
+
+            app.UseMvc(routes=> 
             {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapRoute("Default", "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
